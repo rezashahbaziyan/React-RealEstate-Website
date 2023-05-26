123d19fadcdcfda184fea86b8596783c13e66970
ujin <rezashahtrader@gmail.com>
2023-05-26 07:24:32 +0330

    hero.jsx

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero-wrapper'>
+        <div className="paddinds innerWidth flexCenter hero-container">
+
+        </div>
+    </section>
+  )
+}
+
+export default Hero
