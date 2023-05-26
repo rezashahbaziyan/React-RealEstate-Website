c75eb0843ae93d358668c1411368f42701068067
ujin <rezashahtrader@gmail.com>
2023-05-26 06:47:27 +0330

    menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className="h-wrapper">
+        <div className="h-container">
+
+            <img src="./logo.png" alt="logo" width={100} />
+            <div className="h-menu">
+                <a href="">Residencies</a>
+                <a href="">Our Value</a>
+                <a href="">Contact us</a>
+                <a href="">Get Started</a>
+                <button className='button'>
+                    <a href="">Contact</a>
+                </button>
+            </div>
+        </div>
+    </section>
   )
 }
 
