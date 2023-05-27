276627e9f899cd2cf484fa3dd057cf60b9743b6d
ujin <rezashahtrader@gmail.com>
2023-05-27 14:44:02 +0330

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <div className="white-gradient"/>
         <Header/>
         <Hero/>
+      </div>
     </div>
   );
 }
