1c4b4d27cd889fd13e2152ece604be2910608ad6
ujin <rezashahtrader@gmail.com>
2023-05-27 13:18:29 +0330

    icon

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Hero.css";
+import {HiLocationMarker} from 'react-icons/hi'
 
 const Hero = () => {
   return (
@@ -8,13 +9,16 @@ const Hero = () => {
 
             <div className="flexColStart hero-left">
               <div className="hero-title">
+                <div className="oreng-circle"/>
                 <h1>Discover <br/> Most Suitable <br/> Property</h1>
               </div>
               <div className="flexColStart hero-des">
                 <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span>
                 <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
               </div>
-              <div className="search-bar">search bar</div>
+              <div className="search-bar">
+                <HiLocationMarker color="var{--blue}" size={25}/>
+              </div>
             </div>
             <div className="flexCenter hero-right">
               <div className="image-container">
