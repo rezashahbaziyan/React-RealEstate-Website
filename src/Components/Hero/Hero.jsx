54605fa4f34369af67fb70993404c34a2179bac6
ujin <rezashahtrader@gmail.com>
2023-05-27 12:58:54 +0330

    h

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,15 +6,15 @@ const Hero = () => {
     <section className='hero-wrapper'>
         <div className="paddinds innerWidth flexCenter hero-container">
 
-            <div className="hero-left">
+            <div className="flexColStart hero-left">
               <div className="hero-title">
                 <h1>Discover <br/> Most Suitable <br/> Property</h1>
               </div>
-              <div className="hero-des">
+              <div className="flexColStart hero-des">
                 <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span>
                 <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
               </div>
-              <div className="search-bar"></div>
+              <div className="search-bar">search bar</div>
             </div>
             <div className="flexCenter hero-right">
               <div className="image-container">
