98b83fde9af119ce1f729fa02e82531e9cca68c8
ujin <rezashahtrader@gmail.com>
2023-05-28 09:55:56 +0330

    5

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,7 +4,12 @@ const Contact = () => {
   return (
     <section className="c-wrapper">
         <div className="paddings innerWidth flexCenter c-container">
-            <div className="c-left"></div>
+            <div className="flexColStart c-left">
+                <span className='orangeText'>our Contacts</span>
+                <span className='primaryText'>Easy to Contact</span>
+                <span className='secondaryText'>Lorem ipsum dolor sit amet consectetur adipisicing 
+                    Lorem ipsum dolor sit. elit. Placeat, enim?</span>
+            </div>
             <div className="c-right">
                 <div className="image-container">
                     <img src="./contact.jpg" alt="" />
