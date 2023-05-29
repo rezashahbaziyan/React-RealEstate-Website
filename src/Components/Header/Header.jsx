8fac696830be0bcb9196c79f62e6696890640bf8
ujin <rezashahtrader@gmail.com>
2023-05-29 10:24:46 +0330

    menu out side click logic

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./Header.css";
 import {BiMenuAltRight} from 'react-icons/bi'
+import OutsideClickHandler from 'react-outside-click-handler';
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
@@ -14,17 +15,23 @@ const Header = () => {
         <div className="flexCenter paddings innerWidth h-container">
 
             <img src="./logo.png" alt="logo" width={100} />
-            <div className="flexCenter h-menu"
-            style={getMenuStyles(menuOpened)}
+            <OutsideClickHandler 
+            onOutsideClick={()=>{
+              setMenuOpened(false);
+            }}
             >
-                <a href="">Residencies</a>
-                <a href="">Our Value</a>
-                <a href="">Contact us</a>
-                <a href="">Get Started</a>
-                <button className='button'>
-                    <a href="">Contact</a>
-                </button>
-            </div>
+              <div className="flexCenter h-menu"
+              style={getMenuStyles(menuOpened)}
+              >
+                  <a href="">Residencies</a>
+                  <a href="">Our Value</a>
+                  <a href="">Contact us</a>
+                  <a href="">Get Started</a>
+                  <button className='button'>
+                      <a href="">Contact</a>
+                  </button>
+              </div>
+            </OutsideClickHandler>
             <div className="menu-icon">
               <BiMenuAltRight size={30} onClick={()=>setMenuOpened((prev)=>!prev)}/>
             </div>
