d0e535f2deb2ede1a6458b61ec8955f56d899f8c
ujin <rezashahtrader@gmail.com>
2023-05-29 09:37:01 +0330

    Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,32 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className="f-wrapper">
+        <div className="paddings innerWith flexCenter f-container">
+            {/* left side */}
+            <div className="flexColStart">
+                <img src="./logo2.png" alt="" width={120}/>
+
+                <span className='secondaryText'>
+                    Lorem ipsum dolor sit amet consectetur <br/>
+                    adipisicing as quaerat explicabo. Assumenda.
+                </span>
+            </div>
+
+            {/* right side */}
+            <div className="flexColStart f-right">
+                <span className='primaryText'>Information</span>
+                <span className='secondaryText'>145 New york, FL 4571</span>
+            
+            <div className="flexCenter f-menu">
+                <span>Property</span>
+                <span>Services</span>
+                <span>Product</span>
+                <span>About Us</span>
+            </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
