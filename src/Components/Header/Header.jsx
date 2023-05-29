ff71e01882300d9a0e4ac1cc5a39133ffe361d6e
ujin <rezashahtrader@gmail.com>
2023-05-29 10:08:04 +0330

    menu responsive logic

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css";
 import {BiMenuAltRight} from 'react-icons/bi'
 
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const getMenuStyles = (menuOpened) =>{
+    if (document.documentElement.clientWidth <= 800) {
+      return {right: !menuOpened && "-100%"}
+    }
+  }
   return (
     <section className="h-wrapper">
         <div className="flexCenter paddings innerWidth h-container">
 
             <img src="./logo.png" alt="logo" width={100} />
-            <div className="flexCenter h-menu">
+            <div className="flexCenter h-menu"
+            style={getMenuStyles(menuOpened)}
+            >
                 <a href="">Residencies</a>
                 <a href="">Our Value</a>
                 <a href="">Contact us</a>
@@ -18,7 +26,7 @@ const Header = () => {
                 </button>
             </div>
             <div className="menu-icon">
-              <BiMenuAltRight size={30}/>
+              <BiMenuAltRight size={30} onClick={()=>setMenuOpened((prev)=>!prev)}/>
             </div>
         </div>
     </section>
