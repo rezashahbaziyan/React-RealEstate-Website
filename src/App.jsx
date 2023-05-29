8104ea83de15118c337e224262e14c078431ae32
ujin <rezashahtrader@gmail.com>
2023-05-29 09:26:44 +0330

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Residences from "./Components/Residences/Residences";
 import Value from "./Components/Value/Value";
 import Contact from "./Components/Contact/Contact";
 import GetStarted from "./Components/GetStarted/GetStarted";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Value/>
       <Contact/>
       <GetStarted/>
+      <Footer/>
     </div>
   );
 }
