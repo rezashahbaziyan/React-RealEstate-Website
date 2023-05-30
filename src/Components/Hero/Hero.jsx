332a39dcf0e1e73d07097f87bb68c7dde293c3ab
ujin <rezashahtrader@gmail.com>
2023-05-30 06:34:33 +0330

    h1 animation

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Hero.css";
 import {HiLocationMarker} from 'react-icons/hi'
 import CountUp from 'react-countup';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
@@ -11,7 +12,16 @@ const Hero = () => {
             <div className="flexColStart hero-left">
               <div className="hero-title">
                 <div className="oreng-circle"/>
-                <h1>Discover <br/> Most Suitable <br/> Property</h1>
+                <motion.h1
+                initial={{y:"2rem",opacity:0}}
+                animate={{y:0,opacity:1}}
+                transition={{
+                  duration:2,
+                  type:"spring"
+                }}
+                >
+                  Discover <br/> Most Suitable <br/> Property
+                </motion.h1>
               </div>
               <div className="flexColStart hero-des">
                 <span className='secondaryText'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span>
