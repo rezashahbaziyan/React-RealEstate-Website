e4a0e951f1f0d6f4379c21c4ec43a79b7452f166
Reza shahbaziyan <92777471+rezashahbaziyan@users.noreply.github.com>
2023-06-25 22:44:46 +0330

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-import Header from "./Components/Header/Header";
-import Hero from "./Components/Hero/Hero";
-import "./App.css";
-import Companies from "./Components/Companies/Companies";
-import Residences from "./Components/Residences/Residences";
-import Value from "./Components/Value/Value";
-import Contact from "./Components/Contact/Contact";
-import GetStarted from "./Components/GetStarted/GetStarted";
-import Footer from "./Components/Footer/Footer";
+
 
 function App() {
   return (
